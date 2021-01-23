2f598f86dc770c2ee4a65c5142573051736d4c9a
Michael Cortez <CortezDevelopement@gmail.com>
2021-01-23 02:26:31 -0700

    added wire power on animation effects

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import DrumsButtons from './DrumsButtons'
 import DrumsInterface from './DrumsInterface'
+import { useSelector } from 'react-redux'
 
 export default () => {
+  const mapState = ({bankState, power}) => ({
+    bank: bankState.bank,
+    power: power.powerOn
+  })
+
+  const { power } = useSelector(mapState)
+
   return (
     <div id="drum-machine" style={{ height: "100%", minHeight: "100vh", width: "100%", minWidth: "100vw", display: "flex", alignItems: "center", justifyContent: "center", background: "#1d1d1d" }}>
       <div style={{
@@ -15,7 +23,7 @@ export default () => {
         height: "80vh",
         backgroundColor: "#1f1f1f",
       }}>
-        <div id="wire">
+        <div id="wire" className={`${power ? "wire-power-on" : ""}`}>
         </div>
 
         <div style={{
